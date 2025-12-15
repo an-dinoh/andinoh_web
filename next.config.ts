65b39bea84864847b9156104bae0219565f48067
travis-ugo <okonicha.ugo@gmail.com>
2025-12-15 03:07:11 +0100

    fix: next js error

--- next.config.ts
+++ next.config.ts
@@ -1,38 +1,6 @@
-// import type { NextConfig } from "next";
-
-// const nextConfig: NextConfig = {
-//   webpack(config, { isServer }) {
-//     // Add SVGR loader for SVG files
-//     config.module.rules.push({
-//       test: /\.svg$/i,
-//       issuer: /\.[jt]sx?$/,
-//       use: ["@svgr/webpack"],
-//     });
-
-//     return config;
-//   },
-//   experimental: {
-//     turbo: {
-//       rules: {
-//         '*.svg': {
-//           loaders: ['@svgr/webpack'],
-//           as: '*.js',
-//         },
-//       },
-//     },
-//   },
-// };
-
-// export default nextConfig;
-
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  // ✅ Allow builds to succeed even if ESLint has errors
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-
   webpack(config) {
     // Add SVGR loader for SVG files
     config.module.rules.push({
@@ -44,13 +12,11 @@ const nextConfig: NextConfig = {
     return config;
   },
 
-  experimental: {
-    turbo: {
-      rules: {
-        "*.svg": {
-          loaders: ["@svgr/webpack"],
-          as: "*.js",
-        },
+  turbopack: {
+    rules: {
+      "*.svg": {
+        loaders: ["@svgr/webpack"],
+        as: "*.js",
       },
     },
   },
