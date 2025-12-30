cf092c3c8e41d734e7ce6fe701351d873bcc57be
travis-ugo <okonicha.ugo@gmail.com>
2025-12-30 16:13:56 +0100

    fix: undefine turbo pack

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  experimental: {
+    turbo: undefined,
+  },
+
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/i,
